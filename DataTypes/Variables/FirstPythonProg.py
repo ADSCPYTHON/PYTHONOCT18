949764731fa4b6bfc497b55d27007a5c4b062c2f
ADSCPYTHON <a.vision2050@hotmail.com>
2018-10-05 13:17:44 -0400

    OCT5THACOMMIT

---
+++ DataTypes/Variables/FirstPythonProg.py
@@ -0,0 +1,29 @@
+
+
+# Creating variables in python
+
+# Creating String Variables
+
+fname = 'Guido'
+mname ="Van"
+lname = """Rossum"""
+software ='''Python 3.6.0 '''
+
+# Accessing variables with the help of print()
+print(fname)
+print("")
+print(mname)
+print("")
+print(lname)
+print("")
+print(software)
+
+# Verify the variable data type using type();
+
+print(type(fname))
+print("")
+print(type(mname))
+print("")
+print(type(lname))
+print("")
+print(type(software))
