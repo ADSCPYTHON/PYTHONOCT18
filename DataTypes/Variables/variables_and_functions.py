c011e526713f41dfefca88683b1ca0c057558975
ADSCPYTHON <a.vision2050@hotmail.com>
2018-10-09 12:59:18 -0400

    commitoct19noon

---
+++ DataTypes/Variables/variables_and_functions.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3.6
+#Declaring or creating variables
+var = 10
+var1 = 10.25
+var3 = "Django"
+var4 = """Anaconda Spyder"""
+var5 = '''Pychram Spyder'''
+
+
+"""
+Multi-Line
+Comments
+using Double triples
+"""
+
+'''
+Multi-line 
+comments
+using single triples
+'''
+
+#Accesing variables
+
+print(var,type(var),id(var))
+
+print(var1,type(var1),id(var1))
+
+print(var3,type(var3),id(var3))
+
+print(var4,type(var4),id(var4))
+
+print(var5,type(var5),id(var5))
+
+
+#Creating varables and accessing them using print()
+
+Abc, cde, doll, tools, idel=10, 20.10, 3+4j, "pycharm", "spider"
+
+print(Abc, type(Abc), id(Abc))
+
+print(cde, type(cde), id(cde))
+
+print(doll, type(doll), id(doll))
+
+print(tools, type(tools), id(type))
+
+print(idel, type(idel), id(idel))
+
+
+#Creating variables and accessing them using print
+
+var10=var20=var30=100
+
+print(var10, type(var10), id(var10))
+
+print(var20, type(var20), id(var20))
+
+print(var30, type(var30), id(var30))
+
